c18c54556510f5ef0d5131668077adfa3ee29ea5
이정민 <ijeong0416@gmail.com>
2024-02-07 12:24:12 +0900

    init

---
+++ ddobii/src/main/java/com/ddobii/back/ddobii/DdobiiApplication.java
@@ -0,0 +1,13 @@
+package com.ddobii.back.ddobii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdobiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdobiiApplication.class, args);
+	}
+
+}
